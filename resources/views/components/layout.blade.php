383c517c8d6346d36fcc7852e38f7637cc6c6192
jas-kaur <69312090+jas-kaur@users.noreply.github.com>
2022-09-30 06:12:49 +0800

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -48,17 +48,17 @@
                     >
                 </li>
                 <li>
-                    <a href="register.html" class="hover:text-laravel"
+                    <a href="/employee" class="hover:text-laravel"
                         > Employee</a
                     >
                 </li>
                 <li>
-                    <a href="register.html" class="hover:text-laravel"
+                    <a href="/register" class="hover:text-laravel"
                         ><i class="fa-solid fa-user-plus"></i> Register</a
                     >
                 </li>
                 <li>
-                    <a href="login.html" class="hover:text-laravel"
+                    <a href="/login" class="hover:text-laravel"
                         ><i class="fa-solid fa-arrow-right-to-bracket"></i>
                         Login</a
                     >
