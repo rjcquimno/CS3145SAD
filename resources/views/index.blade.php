ecd0ff674198a74e90c9855a7e9ac352515fc8df
jas-kaur <69312090+jas-kaur@users.noreply.github.com>
2022-10-15 05:09:24 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,100 +10,37 @@
            <div class="col-lg-2">
                 <div class="row flex flex-col justify-center align-center text-center mb-6">
                     <div class="col-lg-2">
-                    <h1 class="text-3xl font-bold uppercase text-black">
-                            <a href="/salesorder" class="hover:text-laravel pt-1 py-1 px-4 border-8 border-black">
-                                &nbsp &nbsp Cashier &nbsp &nbsp
+                    <h1 class="text-4xl font-bold uppercase text-black">
+                            <a href="/salesorder" class="hover:text-laravel pt-1 py-2 px-5 border-8 border-black">
+                                &nbsp&nbsp&nbsp&nbsp Cashier &nbsp&nbsp&nbsp&nbsp&nbsp
+                            </a><br><br>
+                            <a href="/saleshistory" class="hover:text-laravel py-2 px-5 border-8 border-black">
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Sales &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                            </a><br><br>
+                            <a href="/inventory" class="hover:text-laravel py-2 px-5 border-8 border-black">
+                                &nbsp Inventory &nbsp
+                            </a><br><br>
+                           <a href="/procurement" class="hover:text-laravel py-2 px-5 border-8 border-black">
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Stock&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                            </a><br><br>
+                            <a href="/supplier" class="hover:text-laravel py-2 px-5 border-8 border-black">
+                                &nbsp&nbsp&nbsp&nbsp Supplier&nbsp&nbsp&nbsp&nbsp
+                            </a><br><br>
+                            <a href="/employee" class="hover:text-laravel py-2 px-5 border-8 border-black">
+                                &nbsp&nbsp&nbsp Employee&nbsp&nbsp&nbsp
+                            </a><br><br>
+                            <a href="/showattendancecreator/{{$id=Auth::id();}}" class="hover:text-laravel py-2 px-5 border-8 border-black">
+                                Attendance
                             </a>
+
+                           
                         
                     </h1>
                     </div>
                 </div>
             </div>
-
-            <br/>
-
-            <div class="col-lg-2">
-                <div class="row flex flex-col justify-center align-center text-center mb-4">
-                    <div class="col-lg-2 ">
-                        <h1 class="text-3xl font-bold uppercase text-black">
-                            <a href="/saleshistory" class="hover:text-laravel px-1 border-8 border-black">
-                                &nbsp &nbsp &nbsp &nbsp Sales &nbsp &nbsp &nbsp &nbsp
-                            </a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-
-            <br/>
-
-            <div class="col-lg-2">
-                <div class="row flex flex-col justify-center align-center text-center mb-4">
-                    <div class="col-lg-2 ">
-                        <h1 class="text-3xl font-bold uppercase text-black">
-                            <a href="/inventory" class="hover:text-laravel border-8 border-black pr-3 pl-3">
-                                &nbsp Inventory &nbsp
-                            </a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-
-            <br/>
-
-            <div class="col-lg-2">
-                <div class="row flex flex-col justify-center align-center text-center mb-4">
-                    <div class="col-lg-2 ">
-                        <h1 class="text-3xl font-bold uppercase text-black">
-                            <a href="/procurement" class="hover:text-laravel border-8 border-black">
-                                &nbsp &nbsp &nbsp &nbsp STOCK &nbsp &nbsp &nbsp &nbsp
-                            </a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-
-            <br/>
-
-            <div class="col-lg-2">
-                <div class="row flex flex-col justify-center align-center text-center mb-4">
-                    <div class="col-lg-2 ">
-                        <h1 class="text-3xl font-bold uppercase text-black">
-                            <a href="/supplier" class="hover:text-laravel border-8 border-black pr-3 pl-3">
-                                &nbsp  &nbsp Supplier &nbsp &nbsp
-                            </a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-
-            <br/>
-
-            <div class="col-lg-2">
-                <div class="row flex flex-col justify-center align-center text-center mb-4">
-                    <div class="col-lg-2 ">
-                        <h1 class="text-3xl font-bold uppercase text-black">
-                            <a href="/employee" class="hover:text-laravel border-8 px-1 border-black">
-                                &nbsp &nbsp Employee &nbsp &nbsp
-                            </a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-            <br/>
-            <div class="col-lg-2">
-                <div class="row flex flex-col justify-center align-center text-center mb-4">
-                    <div class="col-lg-2 ">
-                        <h1 class="text-3xl font-bold uppercase text-black">
-                            <a href="/showattendancecreator/{{$id=Auth::id();}}" class="hover:text-laravel border-8 border-black">
-                                &nbsp Attendance &nbsp
-                            </a>
-                        </h1>
-                    </div>
-                </div>
-            </div>
+        </div>
     </div>
-</div>
-<div>
 
     @endauth
 </x-layout>
